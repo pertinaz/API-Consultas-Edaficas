e2750f2f05a4b315fdccbb0e145719da65525585
Agudelo <87156289+pertinaz@users.noreply.github.com>
2024-03-07 17:01:00 -0500

    Update Querys.py

--- API/Querys.py
+++ API/Querys.py
@@ -1,14 +1,20 @@
 import pandas as pd
 from sodapy import Socrata
 import json
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *
 
 client = Socrata("www.datos.gov.co",None)
 
 dataset_id ="ch4u-f3i5"
-results = client.get(dataset_id,departamento="RISARALDA")
+results = client.get(dataset_id, limit="10000") #Limite estático para no generar un fallo por sobrecarga de datos
 
 # Convert to pandas DataFrame
 results_df=pd.DataFrame.from_records(results)
 
 with open('QueryResults.json','w') as file:
     json.dump(results,file)
+
+with open('QueryResults.json','r') as file:
+    QueryResults = json.load(file)
