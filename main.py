a5e7d7873f5f753e765b36711cbecedc85df7281
Agudelo <87156289+pertinaz@users.noreply.github.com>
2024-03-14 02:09:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import ui
+import api
 
+def main():
+
+    print("CONSULTAS EDAFICAS COLOMBIA")
+    department,municipality,crop,limit = ui.requestedInformation()
+    results_df = api.getResults(department,municipality,crop,limit)
+    
+    #Show the info
+    ui.ShowFilteredInformation(results_df)
+
+    #Show median
+    ui.phMedian(results_df)
+    ui.matchMedian(results_df)
+    ui.potassiumMedian(results_df)
+
+
+
+main()
